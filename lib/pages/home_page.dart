bc0b4594b4a80d2e9352d4b031f33acbeaff4445
Toretay <abdymvzhit@gmail.com>
2024-11-08 23:33:58 +0500

    first beta version of the calculator

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -1,4 +1,4 @@
-// ignore_for_file: prefer_const_constructors
+// ignore_for_file: prefer_const_constructors, prefer_const_literals_to_create_immutables
 
 import 'package:flutter/material.dart';
 
@@ -12,6 +12,29 @@ class HomePage extends StatefulWidget {
 class _HomePageState extends State<HomePage> {
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    return Scaffold(
+      body: Column(
+        crossAxisAlignment: CrossAxisAlignment.stretch,
+        children: [
+          Expanded(child: Container(
+            decoration: BoxDecoration(
+              color: Colors.black
+            ),
+            child: Text("156"))),
+          Expanded(
+            flex: 2,
+            child: GridView.builder(
+              itemCount: 16,
+              gridDelegate:
+                  SliverGridDelegateWithFixedCrossAxisCount(crossAxisCount: 4),
+              itemBuilder: (context, index) {
+                return ElevatedButton(
+                    onPressed: () {}, child: Text(index.toString()));
+              },
+            ),
+          )
+        ],
+      ),
+    );
   }
 }
