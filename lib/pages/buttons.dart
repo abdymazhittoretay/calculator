861fe5f18225775eda94c9f4dbc5584eba693cae
Toretay <abdymvzhit@gmail.com>
2024-11-11 17:47:50 +0500

    create buttons with gesture and container

--- lib/pages/buttons.dart
+++ lib/pages/buttons.dart
@@ -1,16 +1,31 @@
+// ignore_for_file: avoid_unnecessary_containers
+
 import 'package:flutter/material.dart';
 
 class Buttons extends StatelessWidget {
-  final Color color;
+  final Color buttonColor;
   final Color textColor;
   final String buttonText;
 
-  const Buttons({super.key, required this.color, required this.textColor, required this.buttonText});
+  const Buttons(
+      {super.key,
+      required this.buttonColor,
+      required this.textColor,
+      required this.buttonText});
 
   @override
   Widget build(BuildContext context) {
     return GestureDetector(
-      child: Container(),
+      child: ClipRRect(
+          borderRadius: BorderRadius.circular(50.0),
+          child: Container(
+            color: buttonColor,
+            child: Center(
+                child: Text(
+              buttonText,
+              style: TextStyle(color: textColor),
+            )),
+          )),
     );
   }
 }
